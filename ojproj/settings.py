a3211f7aa618a0f7045d8baaf32064ab63b68701
Homi Mickey <69302617+homimickey@users.noreply.github.com>
2021-08-10 19:47:56 +0530

    Update settings.py

--- ojproj/settings.py
+++ ojproj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(yk^rji_i@nb0rjs$!z+yv&@r_qq0mul8mco$q9ea^onqfe20y'
+SECRET_KEY = "veryverysecret"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
